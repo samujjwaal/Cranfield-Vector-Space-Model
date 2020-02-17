62e35398696a299cb5195c9ee8f4308358d2ac02
Samujjwaal Dey <sdey9@uic.edu>
2020-02-17 05:02:27 -0600

    Tokenized documents and query
    1. Preprocessed all documents and the queries file.
    2. Removed stopwords, performed stemming, removed punctuations and numbers & split on whitespaces

--- HW2.py
+++ HW2.py
@@ -17,60 +17,98 @@
 #
 # ## CS 582 Information Retrieval : Homework 1
 
+# ## Imports and Initializations
+
+# + init_cell=true
+# Importing dependancy libraries
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 import os
 import pandas as pd
 import re
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from bs4 import BeautifulSoup
-
-#Importing the NLTK Stopwords
-import nltk
-nltk.download('stopwords')
 stop_list = stopwords.words('english')
-print(stop_list)
 
+
+# + init_cell=true
+# Declaring variables for file path
 in_path = 'cranfieldDocs'
 out_path = 'preprocessed_cranfieldDocs'
-os.mkdir(out_path)
-filenames = os.listdir(path)   #To generate file path
+
+query = 'queries.txt'
+preproc_query = 'new_queries.txt'
+
+# Checking if the preprocessed docs folder exists already
+if not os.path.isdir(out_path):
+    os.mkdir(out_path)
+
+# Getting all filenames from the docs folder
+filenames = os.listdir(in_path)  # To generate file path
 # print(filenames)
 
-filepath = in_path + '/' + filenames[0]
-file = open(filepath)
-data = file.read()
-file.close()
+# Initiallizing Porter Stemmer object
+st = PorterStemmer()
 
-soup = BeautifulSoup(data)
-title = soup.findAll('title')
-text = soup.findAll('text')
-# for item in title:
-#     print(item.get_text())
 
-# +
-# print(soup.prettify())
 # -
+
+# ## Preprocessing the documents
+
+def tokenize(data):
+    lines = data.lower()  # converting to lower case
+    # removing punctuations by using regular expression
+    lines = re.sub('[^A-Za-z]+', ' ', lines)
+    tokens = lines.split()
+    # removing stop words from the text
+    clean_tokens = [word for word in tokens if word not in stop_list]
+    stem_tokens = [st.stem(word)
+                   for word in clean_tokens]  # stemming the words
+    clean_stem_tokens = [word for word in stem_tokens if word not in stop_list]
+    clean_stem_tokens = ' '.join(map(str,  clean_stem_tokens))
+    return clean_stem_tokens
+
+
+def extractTokens(beautSoup, tag):
+    textData = beautSoup.findAll(tag)
+    textData = ''.join(map(str, textData))
+    textData = textData.replace(tag, '')
+    textData = tokenize(textData)
+    return textData
+
 
 for fname in filenames:
     infilepath = in_path + '/' + fname
     outfilepath = out_path + '/' + fname
-    with open(outfilepath, 'w') as outfile:    
-        with open(infilepath) as infile:
-            soup = BeautifulSoup(infile.read())
-            title = soup.findAll('title')
-            text = soup.findAll('text')
-            for item in title:
-                outfile.write(item.get_text())
-            for item in text:
-                 outfile.write(item.get_text())
-        infile.close()
-outfile.close()
+    with open(infilepath) as infile:
+        with open(outfilepath, 'w') as outfile:
+            fileData = infile.read()
+            soup = BeautifulSoup(fileData)
+            title = extractTokens(soup, 'title')
+            text = extractTokens(soup, 'text')
+            outfile.write(title)
+            outfile.write(" ")
+            outfile.write(text)
+        outfile.close()
+    infile.close()
 
-filepath = out_path + '/' + filenames[0]
-file = open(filepath)
-data = file.read()
-data
+q = open(query)
+new_q = open(preproc_query, 'w')
+text = q.readlines()
+for line in text:
+    if(line != text[-1]):
+        query_tokens = tokenize(line)
+        new_q.write(query_tokens + '\n')
+    else:
+        query_tokens = tokenize(line)
+        new_q.write(query_tokens)
+
+# +
+# filepath = out_path + '/' + filenames[0]
+# file = open(filepath)
+# data = file.read()
+# data
+# -
 
 
